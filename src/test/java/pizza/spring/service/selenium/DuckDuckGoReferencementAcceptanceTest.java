10a535a175d86c71a53ee3696cbf75fa852835b6
HuuTai33 <44099208+HuuTai33@users.noreply.github.com>
2020-05-07 19:34:30 +0200

    modif Selenium Test

--- src/test/java/pizza/spring/service/selenium/DuckDuckGoReferencementAcceptanceTest.java
+++ src/test/java/pizza/spring/service/selenium/DuckDuckGoReferencementAcceptanceTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
